998b847772c28d09c64aaee5a63c78ed8c10705f
Leandro Hideki <htwleandro@gmail.com>
2025-07-09 09:19:35 -0300

    Delete src/main/java/com/example/Books/microsservice_Books_main directory

--- src/main/java/com/example/Books/microsservice_Books_main/enums/ContentRatingEnum.java
+++
@@ -1,9 +0,0 @@
-package com.example.Books.microsservice_Books_main.enums;
-
-public enum ContentRatingEnum {
-    G_GENERAL_AUDIENCE,
-    PG,
-    PG_13,
-    R,
-    NC_17
-}
--- src/main/java/com/example/Books/microsservice_Books_main/repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Books.microsservice_Books_main.repositories;
-
-import com.example.Books.microsservice_Books_main.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
